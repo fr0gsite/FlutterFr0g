45dc1971779ca2d050cb89aa9d9ae329bee51967
AnSieger <AnSieger@mailbox.org>
2025-04-19 19:42:24 +0200

    prototype of ageverification quiz

---
+++ lib/widgets/ageverification/quiz.dart
@@ -0,0 +1,102 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fr0gsite/config.dart';
+import 'package:fr0gsite/widgets/ageverification/quizcard.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+
+class _QuizState extends State<Quiz> {
+
+  Future<Uint8List> loadAssetAsUint8List(String path) async {
+  final byteData = await rootBundle.load(path);
+  return byteData.buffer.asUint8List();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SingleChildScrollView(
+        child: SizedBox(
+          width: 600,
+          height: 800,
+          child: Material(
+              color: AppColor.nicegrey,
+              shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  side: BorderSide(
+                    color: Colors.white,
+                    width: 6,
+                  )),
+              child: Scaffold(
+                backgroundColor: Colors.transparent,
+                resizeToAvoidBottomInset: true,
+                appBar: AppBar(
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
+                  leading: IconButton(
+                    icon: const Icon(Icons.close, color: Colors.white),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+                body: Scaffold(
+                  body: Stack(
+                    children: [
+                      Image.asset(
+                      'assets/images/ageverification/quiz1.gif',
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                      height: double.infinity,
+                      ),
+                      Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: FutureBuilder(future: loadAssetAsUint8List('assets/images/ageverification/vhs.png'), builder: 
+                          (context, snapshot) {
+                            if (snapshot.connectionState == ConnectionState.done) {
+                              return QuizCard(
+                                question: "Was ist das?",
+                                answers: [
+                                  "VHS Kasette",
+                                  "CD",
+                                  "Floppy Disk",
+                                  "USB Stick"],
+                                imageData: snapshot.data as Uint8List,
+                                onAnswerSelected: (index) {
+                                  // Handle answer selection
+                                  print("Selected answer: ${index}");
+                                },
+                              );
+                            } else {
+                              return const Center(child: CircularProgressIndicator());
+                            }
+                          },
+                        ),
+                      ),
+                      ),
+                  
+                    ],
+                  ),
+                ),
+              )),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/ageverification/quizcard.dart
@@ -0,0 +1,99 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+
+class QuizCard extends StatelessWidget {
+  final String question;
+  final List<String> answers;
+  final void Function(int)? onAnswerSelected;
+
+  // optinal Image as Uint8List
+  final Uint8List? imageData;
+
+  const QuizCard({
+    Key? key,
+    required this.question,
+    required this.answers,
+    this.imageData,
+    this.onAnswerSelected,
+  })  : assert(answers.length == 4),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            color: Colors.black54,
+            child: Text(
+              question,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const SizedBox(height: 24),
+          if (imageData != null)
+            Image.memory(
+              imageData!,
+              fit: BoxFit.cover,
+              width: 300,
+              height: 300,
+            ),
+          const SizedBox(height: 16),
+          GridView.builder(
+            shrinkWrap: true,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, // Two elements per row
+              crossAxisSpacing: 12,
+              mainAxisSpacing: 12,
+              childAspectRatio: 3,
+            ),
+            itemCount: answers.length,
+            itemBuilder: (context, index) {
+              final letter = String.fromCharCode(65 + index);
+              return GestureDetector(
+                onTap: () => onAnswerSelected?.call(index),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.blue[900],
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(color: Colors.white, width: 2),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 8,
+                  ),
+                  child: Row(
+                    children: [
+                      Text(
+                        '$letter:',
+                        style: const TextStyle(
+                          color: Colors.orange,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      Expanded(
+                        child: Text(
+                          answers[index],
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
+      );
+  }
+}
